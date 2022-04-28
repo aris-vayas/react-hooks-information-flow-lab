f71dc3f7483d9a27ebe9f131d9cc99b30c084f23
Aris Vayas <aris.vayas@gmail.com>
2022-04-28 16:36:43 -0400

    commit tree 9b5a28d6745d3feea0c810ae2fb1c0299d455226

---
+++ src/components/Filter.js
@@ -0,0 +1,13 @@
+export default function Filter({selectedCategory, onCategoryChange}){
+
+return (
+
+<select name="filter" onChange={onCategoryChange}>
+
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+ </select>
+)
+}
